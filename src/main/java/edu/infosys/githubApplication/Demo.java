0114ba724fb3a928715319dce04403ec146ed09e
Greeshma-997 <greeshu997@gmail.com>
2026-03-24 12:46:14 +0530

    Added File

---
+++ src/main/java/edu/infosys/githubApplication/Demo.java
@@ -0,0 +1,11 @@
+package edu.infosys.githubApplication;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Namaste");
+
+	}
+
+}
